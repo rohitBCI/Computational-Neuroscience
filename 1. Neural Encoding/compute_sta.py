e0b31be58540dbdb68b060e21dbc7e74310e7395
Rohit Vijayakumar <rohitmacx@gmail.com>
2021-02-20 22:56:11 +0100

    Averaged sta over num_spikes

--- 1. Neural Encoding/compute_sta.py
+++ 1. Neural Encoding/compute_sta.py
@@ -29,7 +29,7 @@ def compute_sta(stim, rho, num_timesteps):
     # Fill in this value. Note that you should not count spikes that occur
     # before 300 ms into the recording.
     num_spikes = len(spike_times)
-    print(num_spikes)
+    print(f'Number of spikes{num_spikes}')
     # Compute the spike-triggered average of the spikes found.
     # To do this, compute the average of all of the vectors
     # starting 300 ms (exclusive) before a spike and ending at the time of
