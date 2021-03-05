8618485b6d094f8240928bd29ad5f2d745745392
Rohit Vijayakumar <rohitmacx@gmail.com>
2021-03-05 16:40:26 +0100

    Added Networks that learn

--- 5. Computing with networks/question9.py
+++ 5. Computing with networks/question9.py
@@ -26,8 +26,12 @@ print(f"h:{h}") # 5x1
 ev, e = np.linalg.eig(M) 
 print(f"ev:{ev}") # 5,
 print(f"e:{e}")   # 5x5
+ev = ev.reshape(5,1)
 
-print(h.shape)
-v_ss= (np.dot((np.dot(np.transpose(h), e) / (1 - ev)),e))
+h_e = np.dot(np.transpose(h),e)
+
+h_e_ev = h_e/np.transpose(1-ev)
+v_ss= np.dot(h_e_ev,e)
+print(v_ss.shape)
 print(np.sum(v_ss, axis=0))
 # [0.57468284 0.46104003 0.5556308  0.35789389 0.33646672]
